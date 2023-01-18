9e54166d1ff0ea4730c633e0d521992f09ce249a
Natalia B <90588475+nanab210@users.noreply.github.com>
2023-01-18 11:39:25 -0300

    Validate Player Guesses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessLetterButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const progress = document.querySelector(".word-in-progress");
@@ -8,10 +8,12 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
+    console.log(letter);
     placeholderLetters.push("●");
   }
 
@@ -22,7 +24,39 @@ placeholder(word);
 
 guessLetterButton.addEventListener("click", function (e) {
   e.preventDefault();
+
+  message.innerText = "";
+
   const guesses = letterInput.value;
-  console.log(guesses);
+
+  const goodGuess = playerInput(guesses);
+
+  if (goodGuess) {
+    makeGuess(guesses);
+  }
   letterInput.value = "";
 });
+
+const playerInput = function (input) {
+  const acceptedLetter = /[a-zA-Z]/;
+
+  if (input.length === 0) {
+    message.innerText = "Please enter a letter";
+  } else if (input.length > 1) {
+    message.innerText = "Please enter only one letter at a time";
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = "Please enter only letters from A to Z";
+  } else {
+    return input;
+  }
+};
+
+const makeGuess = function (guesses) {
+  guesses = guesses.toUpperCase();
+  if (guessedLetters.includes(guesses)) {
+    message.innerText = "You already entered that letter, please try again";
+  } else {
+    guessedLetters.push(guesses);
+    console.log(guessedLetters);
+  }
+};
